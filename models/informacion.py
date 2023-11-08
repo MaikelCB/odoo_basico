55ad3a18edac50e0e10d79a98d026e0832ac6ad8
a23maikelcb <maikelcb.96@gmail.com>
2023-11-08 14:24:58 +0100

    selection and boolean

--- models/informacion.py
+++ models/informacion.py
@@ -11,5 +11,7 @@ class informacion(models.Model):
     description = fields.Text(string="A Descripción:")
     alto_en_cms = fields.Integer(string="Alto en centímetros")
     longo_en_cms = fields.Integer(string="Longo en centímetros")
-    ancho_en_cms = fields.Integer(string="Ancho en centímetros:")
+    ancho_en_cms = fields.Integer(string="Ancho en centímetros")
     peso = fields.Float(digits=(6, 2), default=2.7, string="Peso en Kg")
+    sexo = fields.Selection([("Hombre", "Hombre"), ("Mujer", "Mujer"), ("Otros", "Otros")], string="Sexo")
+    autorizado = fields.Boolean(default=False, string="¿Autorizado?")
