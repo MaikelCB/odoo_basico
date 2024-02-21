b5cc9bf954ee12145d891babd91f8f8e19f0082d
a23maikelcb <maikelcb.96@gmail.com>
2024-02-21 14:23:30 +0100

    21/02/24 email y locale

--- models/informacion.py
+++ models/informacion.py
@@ -3,6 +3,8 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
+import os
+
 
 class informacion(models.Model):
     _name = 'odoo_basico.informacion'
@@ -66,3 +68,44 @@ class informacion(models.Model):
 
     def _cambia_campo_sexo(self, rexistro):
         rexistro.sexo_traducido = "Hombre"
+
+    def envio_email(self):
+        meu_usuario = self.env.user
+        # mail_de     Odoo pon o email que configuramos en gmail para facer o envio
+        mail_reply_to = meu_usuario.partner_id.email  # o enderezo email que ten asociado o noso usuario
+        mail_para = 'braisiessg@gmail.com'  # o enderezo email de destino
+        mail_valores = {
+            'subject': 'Aquí iría o asunto do email ',
+            'author_id': meu_usuario.id,
+            'email_from': mail_reply_to,
+            'email_to': mail_para,
+            'message_type': 'email',
+            'body_html': 'Aquí iría o corpo do email cos datos por exemplo de "%s" ' % self.descripcion,
+        }
+        mail_id = self.env['mail.mail'].create(mail_valores)
+        mail_id.sudo().send()
+        return True
+
+    ''' 
+    def ver_contexto(self):  # Este método é chamado dende un botón de informacion.xml
+        for rexistro in self:
+            # Ao usar warning temos que importar a libreria mediante from odoo.exceptions import Warning
+            # Importamos tamén a libreria os mediante import os
+            raise Warning(
+                'Contexto: %s Ruta: %s Contido %s' % (rexistro.env.context, os.getcwd(), os.listdir(os.getcwd())))
+            # env.context é un diccionario  https://www.w3schools.com/python/python_dictionaries.asp
+        return True
+    '''
+
+    def ver_contexto(self):  # Este método é chamado dende un botón de informacion.xml
+        for rexistro in self:
+            # Ao usar warning temos que importar a libreria mediante from odoo.exceptions import Warning
+            # Importamos tamén a libreria os mediante import os
+            # raise Warning('Contexto: %s Ruta: %s Contido do directorio %s' % (rexistro.env.context, os.getcwd(), os.listdir(os.getcwd())))
+
+            # Warning is deprecated por iso usamos ValidationError
+
+            raise ValidationError('Contexto: %s Ruta: %s Contido do directorio %s' % (
+                rexistro.env.context, os.getcwd(), os.listdir(os.getcwd())))
+            # env.context é un diccionario  https://www.w3schools.com/python/python_dictionaries.asp
+        return True
